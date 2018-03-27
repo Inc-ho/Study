87e9ab8b66c4a4508080766538c44242a620ec5e
Inc-ho <878014750@qq.com>
2018-03-27 15:33:22 +0800

    拉取项目再提交

---
+++ src/main/java/com/ht/springboot/imooc/Test/GitTest.java
@@ -0,0 +1,13 @@
+package com.ht.springboot.imooc.Test;
+
+/**
+ * Create By Min
+ * 2018/3/27
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("拉取项目再提交！");
+    }
+
+}
