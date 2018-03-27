0cd9dc214fabf3d4269817677f0a5e106a71d993
Inc-ho <878014750@qq.com>
2018-03-27 15:47:59 +0800

    创建分支再提交测试

---
+++ src/main/java/com/ht/springboot/imooc/Test/BranchTest.java
@@ -0,0 +1,13 @@
+package com.ht.springboot.imooc.Test;
+
+/**
+ * Create By Min
+ * 2018/3/27
+ */
+public class BranchTest {
+
+    public static void main(String[] args) {
+        System.out.println("创建分支提交测试！");
+    }
+
+}
